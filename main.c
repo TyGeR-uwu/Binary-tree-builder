17e22506f73cfd6b9de7f59ffd565dfe97f70573
TyGeR <laggerspeed880@gmail.com>
2024-04-16 22:43:19 +0300

    Тест

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ void tree_draw(Node* tree) {
 	tree_divide_by_levels(tree, 0, &plevels); //asdsad
 
 }//asdasd
-//asdsa
+//asdsadadadsasdasdadsadadadasdsadad
 Node* tree_add(Node * tree, Data d) {
 	if (tree == NULL) {
 		Node* p = malloc(sizeof(Node));
